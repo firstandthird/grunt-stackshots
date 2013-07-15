b57b8c453d25b1f7af729fda18dcebc5d8d67855
Antonio Laguna <antonio.matias@gamesys.co.uk>
2013-07-15 17:21:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# grunt-browser-screenshot
+
+> The best Grunt plugin ever.
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-browser-screenshot --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-browser-screenshot');
+```
+
+## The "browser_screenshot" task
+
+### Overview
+In your project's Gruntfile, add a section named `browser_screenshot` to the data object passed into `grunt.initConfig()`.
+
+To be added...
---
+++ tasks/browser_screenshot.js
@@ -0,0 +1,94 @@
+/*
+ * grunt-browser-screenshot
+ * https://github.com/antonio.matias/grunt-browser-screenshot
+ *
+ * Copyright (c) 2013 Antonio Laguna
+ * Licensed under the MIT license.
+ */
+"use strict";
+
+module.exports = function(grunt) {
+  var authFile = '.bspass';
+
+  var fs = require('fs')
+    , url = require('url')
+    , BrowserScreenshot = require('browserscreenshot')
+    , file = grunt.file;
+
+  function getAuthData(authKey) {
+    var temp, authData = null;
+
+    if (authKey && fs.existsSync(authFile)) {
+      temp = file.readJSON(authFile);
+      if (typeof temp[authKey] !== "undefined"){
+        authData = temp[authKey];
+      }
+    }
+    return authData;
+  }
+
+  function arrayToLower(array) {
+    var result = null;
+
+    if (array instanceof Array){
+      result = array.join(',').toLowerCase().split(',');
+    }
+
+    return result
+  }
+
+  grunt.registerMultiTask('browser_screenshot', 'Get screenshots from BrowserStack', function() {
+    var authData = getAuthData(this.data.authKey)
+      , browserStackClient
+      , self = this
+      , done = this.async()
+      , candidateBrowsers = arrayToLower(this.data.browsers)
+      , os = arrayToLower(this.data.os)
+      , devices = arrayToLower(this.data.devices)
+      , orientation = this.data.orientation || 'portrait'
+      , browsers;
+
+    browserStackClient = new BrowserScreenshot({
+      'email' : authData.email,
+      'password' : authData.password,
+      'folder' : this.data.dest
+    }, function(){
+      var preparedOS = browserStackClient.prepareOS(os);
+      browsers = browserStackClient.guessBrowsers(candidateBrowsers, preparedOS.os, preparedOS.osVersions, devices);
+
+      if (self.data.url.paths && self.data.url.paths.length){
+        if (browsers.length){
+          var functions = [];
+
+          self.data.url.paths.forEach(function(path){
+            var fullUrl = url.resolve(self.data.url.base, path);
+            functions.push(function(cb){
+              var request = {
+                url : fullUrl,
+                orientation : orientation,
+                browsers : browsers
+              };
+              grunt.log.subhead('Getting screenshot(s) for %s', fullUrl);
+              browserStackClient.getImages(request,cb);
+            });
+          });
+
+          grunt.util.async.series(functions,function(){
+            grunt.log.ok('All urls have been downloaded!');
+            done();
+          });
+        }
+        else {
+          grunt.log.error('Your settings aren\'t able to get any browser match')
+        }
+      }
+      else {
+        grunt.log.error('You haven\' specified any URLs to get screenshots from.')
+      }
+    });
+
+    if (grunt.errors) {
+      return false;
+    }
+  });
+};
